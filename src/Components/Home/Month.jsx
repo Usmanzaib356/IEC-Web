30fb180364f1b398085aebe535e171782595780b
Usmanzaib356 <110809791+Usmanzaib356@users.noreply.github.com>
2023-07-25 13:53:33 -0700

    #

--- src/Components/Home/Month.jsx
+++ src/Components/Home/Month.jsx
@@ -13,7 +13,7 @@ function Month() {
 
 
 
-                    <div className='pt-10 flex  gap-[5rem] w-full  flex-wrap '>
+                    <div className='pt-10 flex  gap-[5rem] w-full  display-none-m flex-wrap '>
 
                         <div className='w-[154px]  ps-2  py-2'>
 
@@ -91,74 +91,220 @@ function Month() {
 
 
 
-                    <div className='mt-5 flex gap-[5rem] flex-wrap'> 
-
-                        
-
-                    <div className='w-[154px]  ps-2  py-2'>
-
-<div className='bg-[#12125A]  items-center flex  justify-center w-[51px] h-[51px] rounded-[100%]'>
-    <h3 className='fz-26-month'>1</h3>
-</div>
-
-<h4 className='fz-20 py-1 mt-2'>Month 1</h4>
-<h4 className='fz-16-month-2'>Week 1 - week 4</h4>
-<p className='fz-16-p pt-3'>PD Sessions (2hrs)
-</p>
-<p className='fz-16-p'>Self Study (30hrs)
-</p>
-<p className='fz-16-p'>
-    Tech Sessions (6hrs)
-
-</p>
-</div>
+                    <div className='mt-5 flex gap-[5rem] flex-wrap display-none-m'>
 
 
 
+                        <div className='w-[154px]  ps-2  py-2'>
 
-<div className='w-[154px]  ps-2  py-2'>
+                            <div className='bg-[#12125A]  items-center flex  justify-center w-[51px] h-[51px] rounded-[100%]'>
+                                <h3 className='fz-26-month'>1</h3>
+                            </div>
 
-<div className='bg-[#12125A]  items-center flex  justify-center w-[51px] h-[51px] rounded-[100%]'>
-    <h3 className='fz-26-month'>1</h3>
-</div>
+                            <h4 className='fz-20 py-1 mt-2'>Month 1</h4>
+                            <h4 className='fz-16-month-2'>Week 1 - week 4</h4>
+                            <p className='fz-16-p pt-3'>PD Sessions (2hrs)
+                            </p>
+                            <p className='fz-16-p'>Self Study (30hrs)
+                            </p>
+                            <p className='fz-16-p'>
+                                Tech Sessions (6hrs)
 
-<h4 className='fz-20 py-1 mt-2'>Month 1</h4>
-<h4 className='fz-16-month-2'>Week 1 - week 4</h4>
-<p className='fz-16-p pt-3'>PD Sessions (2hrs)
-</p>
-<p className='fz-16-p'>Self Study (30hrs)
-</p>
-<p className='fz-16-p'>
-    Tech Sessions (6hrs)
-
-</p>
-</div>
-
-
-<div className=' flex gap-3  justify-center flex-wrap  bg-[#FFFFFF] px-3 py-4 rounded-xl'>
-
-<div className='w-[12.875rem] text-center flex flex-col justify-center items-center  py-8 rounded-lg hour-cards-color' >
-    <h3 className='fz-28-month-card'>80 hrs</h3>
-    <p className='fz-18-month-card-p'>PD Sessions</p>
-</div>
+                            </p>
+                        </div>
 
 
-<div className='w-[12.875rem] text-center flex flex-col justify-center items-center  py-5 rounded-lg hour-cards-color' >
-    <h3 className='fz-28-month-card'>720 hrs</h3>
-    <p className='fz-18-month-card-p'>Self-Study Sessions</p>
-</div>
 
 
-<div className='w-[12.875rem] text-center flex flex-col justify-center items-center  py-5 rounded-lg hour-cards-color' >
-    <h3 className='fz-28-month-card'>96 hrs</h3>
-    <p className='fz-18-month-card-p'>Self-Study Sessions</p>
-</div>
+                        <div className='w-[154px]  ps-2  py-2'>
 
-</div>
+                            <div className='bg-[#12125A]  items-center flex  justify-center w-[51px] h-[51px] rounded-[100%]'>
+                                <h3 className='fz-26-month'>1</h3>
+                            </div>
+
+                            <h4 className='fz-20 py-1 mt-2'>Month 1</h4>
+                            <h4 className='fz-16-month-2'>Week 1 - week 4</h4>
+                            <p className='fz-16-p pt-3'>PD Sessions (2hrs)
+                            </p>
+                            <p className='fz-16-p'>Self Study (30hrs)
+                            </p>
+                            <p className='fz-16-p'>
+                                Tech Sessions (6hrs)
+
+                            </p>
+                        </div>
+
+
+                        <div className=' flex gap-3  justify-center flex-wrap  bg-[#FFFFFF] px-3 py-4 rounded-xl'>
+
+                            <div className='w-[12.875rem] text-center flex flex-col justify-center items-center  py-8 rounded-lg hour-cards-color' >
+                                <h3 className='fz-28-month-card'>80 hrs</h3>
+                                <p className='fz-18-month-card-p'>PD Sessions</p>
+                            </div>
+
+
+                            <div className='w-[12.875rem] text-center flex flex-col justify-center items-center  py-5 rounded-lg hour-cards-color' >
+                                <h3 className='fz-28-month-card'>720 hrs</h3>
+                                <p className='fz-18-month-card-p'>Self-Study Sessions</p>
+                            </div>
+
+
+                            <div className='w-[12.875rem] text-center flex flex-col justify-center items-center  py-5 rounded-lg hour-cards-color' >
+                                <h3 className='fz-28-month-card'>96 hrs</h3>
+                                <p className='fz-18-month-card-p'>Self-Study Sessions</p>
+                            </div>
+
+                        </div>
                     </div>
 
 
-        
+
+                    {/* For Mobile */}
+
+                    <div className='mt-3 display-none-desktop'>
+
+                        <div className='flex gap-3'>
+                            <div className='w-[154px]  ps-2  py-2'>
+
+                                <div className='bg-[#12125A]  items-center flex  justify-center w-[34px] h-[34px] rounded-[100%]'>
+                                    <h3 className='fz-26-month fz-12-m-agreement'>1</h3>
+                                </div>
+
+                                <h4 className='fz-20 py-1 mt-2 fz-13-m'>Month 1</h4>
+                                <h4 className='fz-16-month-2 fz-12-m-month-red '>Week 1 - week 4</h4>
+                                <p className='fz-16-p pt-3 fz-12-m-user'>PD Sessions (2hrs)
+                                </p>
+                                <p className='fz-16-p fz-12-m-user'>Self Study (30hrs)
+                                </p>
+                                <p className='fz-16-p fz-12-m-user'>
+                                    Tech Sessions (6hrs)
+
+                                </p>
+                            </div>
+
+                            <div className='w-[154px]  ps-2  py-2'>
+
+                                <div className='bg-[#12125A]  items-center flex  justify-center w-[34px] h-[34px] rounded-[100%]'>
+                                    <h3 className='fz-26-month fz-12-m-agreement'>1</h3>
+                                </div>
+
+                                <h4 className='fz-20 py-1 mt-2 fz-13-m'>Month 1</h4>
+                                <h4 className='fz-16-month-2 fz-12-m-month-red '>Week 1 - week 4</h4>
+                                <p className='fz-16-p pt-3 fz-12-m-user'>PD Sessions (2hrs)
+                                </p>
+                                <p className='fz-16-p fz-12-m-user'>Self Study (30hrs)
+                                </p>
+                                <p className='fz-16-p fz-12-m-user'>
+                                    Tech Sessions (6hrs)
+
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className='flex gap-3 mt-3'>
+                            <div className='w-[154px]  ps-2  py-2'>
+
+                                <div className='bg-[#12125A]  items-center flex  justify-center w-[34px] h-[34px] rounded-[100%]'>
+                                    <h3 className='fz-26-month fz-12-m-agreement'>1</h3>
+                                </div>
+
+                                <h4 className='fz-20 py-1 mt-2 fz-13-m'>Month 1</h4>
+                                <h4 className='fz-16-month-2 fz-12-m-month-red '>Week 1 - week 4</h4>
+                                <p className='fz-16-p pt-3 fz-12-m-user'>PD Sessions (2hrs)
+                                </p>
+                                <p className='fz-16-p fz-12-m-user'>Self Study (30hrs)
+                                </p>
+                                <p className='fz-16-p fz-12-m-user'>
+                                    Tech Sessions (6hrs)
+
+                                </p>
+                            </div>
+
+                            <div className='w-[154px]  ps-2  py-2'>
+
+                                <div className='bg-[#12125A]  items-center flex  justify-center w-[34px] h-[34px] rounded-[100%]'>
+                                    <h3 className='fz-26-month fz-12-m-agreement'>1</h3>
+                                </div>
+
+                                <h4 className='fz-20 py-1 mt-2 fz-13-m'>Month 1</h4>
+                                <h4 className='fz-16-month-2 fz-12-m-month-red '>Week 1 - week 4</h4>
+                                <p className='fz-16-p pt-3 fz-12-m-user'>PD Sessions (2hrs)
+                                </p>
+                                <p className='fz-16-p fz-12-m-user'>Self Study (30hrs)
+                                </p>
+                                <p className='fz-16-p fz-12-m-user'>
+                                    Tech Sessions (6hrs)
+
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className='flex gap-3 mt-3'>
+                            <div className='w-[154px]  ps-2  py-2'>
+
+                                <div className='bg-[#12125A]  items-center flex  justify-center w-[34px] h-[34px] rounded-[100%]'>
+                                    <h3 className='fz-26-month fz-12-m-agreement'>1</h3>
+                                </div>
+
+                                <h4 className='fz-20 py-1 mt-2 fz-13-m'>Month 1</h4>
+                                <h4 className='fz-16-month-2 fz-12-m-month-red '>Week 1 - week 4</h4>
+                                <p className='fz-16-p pt-3 fz-12-m-user'>PD Sessions (2hrs)
+                                </p>
+                                <p className='fz-16-p fz-12-m-user'>Self Study (30hrs)
+                                </p>
+                                <p className='fz-16-p fz-12-m-user'>
+                                    Tech Sessions (6hrs)
+
+                                </p>
+                            </div>
+
+                            <div className='w-[154px]  ps-2  py-2'>
+
+                                <div className='bg-[#12125A]  items-center flex  justify-center w-[34px] h-[34px] rounded-[100%]'>
+                                    <h3 className='fz-26-month fz-12-m-agreement'>1</h3>
+                                </div>
+
+                                <h4 className='fz-20 py-1 mt-2 fz-13-m'>Month 1</h4>
+                                <h4 className='fz-16-month-2 fz-12-m-month-red '>Week 1 - week 4</h4>
+                                <p className='fz-16-p pt-3 fz-12-m-user'>PD Sessions (2hrs)
+                                </p>
+                                <p className='fz-16-p fz-12-m-user'>Self Study (30hrs)
+                                </p>
+                                <p className='fz-16-p fz-12-m-user'>
+                                    Tech Sessions (6hrs)
+
+                                </p>
+                            </div>
+                        </div>
+
+
+                        <div className=' flex gap-3 custom-w:mt-5 custom-m:mt-0  justify-center   bg-[#FFFFFF] custom-m:px-3 custom-w:px-2 custom-m:py-4 custom-w:py-2 rounded-xl'>
+
+                            <div className='w-[100px] text-center flex flex-col justify-center items-center px-2  py-4 custom-m:rounded-lg custom-w:rounded-xl  hour-cards-color' >
+                                <h3 className='fz-28-month-card fz-14-m-featured'>80 hrs</h3>
+                                <p className='fz-18-month-card-p fz-11-m py-2'>PD Sessions </p>
+                            </div>
+
+                            
+                            <div className='w-[100px] text-center flex flex-col justify-center items-center px-2  py-4 custom-m:rounded-lg custom-w:rounded-xl  hour-cards-color' >
+                                <h3 className='fz-28-month-card fz-14-m-featured'>720 hrs</h3>
+                                <p className='fz-18-month-card-p fz-11-m py-2'>Self-Study Sessions </p>
+                            </div>
+
+                            
+                            <div className='w-[100px] text-center flex flex-col justify-center items-center px-2  py-4 custom-m:rounded-lg custom-w:rounded-xl  hour-cards-color' >
+                                <h3 className='fz-28-month-card fz-14-m-featured'>96 hrs</h3>
+                                <p className='fz-18-month-card-p fz-11-m py-2'>Tech Sessions </p>
+                            </div>
+
+                        </div>
+
+
+
+
+                    </div>
+
+
 
 
 
